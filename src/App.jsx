9ce9baf00a17d72779618b6e2b4432f929975388
odorizzi1 <jonathanodorizzi@gmail.com>
2021-11-02 00:10:49 -0300

    projeto2

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+import Tasks from './components/Tasks'
+
+import "./App.css";
+import AddTask from "./components/AddTask";
+
+
+const App= () =>{
+ const [tasks, setTasks]= useState([
+   {
+     id:"1",
+     title: "estudar programação",
+     completed:false,
+   },
+   {
+     id:"2",
+     title: "Ler Livros",
+     completed: true,
+   },
+ ]);
+ const handleTaskAddition = (taskTitle) => {
+   const newTasks = [
+     ...tasks,
+     {
+     title: taskTitle,
+     id: Math.random(10),
+     completed: false,}
+   ]
+   
+   
+   setTasks(newTasks);
+ }
+ 
+ return ( 
+ 
+    <div>
+
+            <div className="container">
+              <AddTask handleTaskAddition={handleTaskAddition}/>
+              <Tasks tasks={tasks} />
+     
+   </div>
+
+
+
+
+ </div>)
+
+ };
+export default App;
