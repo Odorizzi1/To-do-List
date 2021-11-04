65e8593c26416cdc8fb930a7cb89c03017ea84d0
odorizzi1 <jonathanodorizzi@gmail.com>
2021-11-03 22:58:31 -0300

    add3

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
-
 import {v4 as uuidv4} from 'uuid';
+
+
 import Tasks from './components/Tasks'
+import AddTask from "./components/AddTask";
+import Header from "./components/Header";
+
 
 import "./App.css";
-import AddTask from "./components/AddTask";
+
 
 
 const App= () =>{
  const [tasks, setTasks]= useState([
    {
      id:"1",
-     title: "estudar programação",
+     title: "estudar ",
      completed:false,
    },
    {
      id:"2",
-     title: "Ler Livros",
+     title: "exercitar",
      completed: true,
    },
  ]);
@@ -27,7 +31,7 @@ const App= () =>{
      return task;
    });
    setTasks(newTasks);
- }
+ };
  
  
  const handleTaskAddition = (taskTitle) => {
@@ -42,21 +46,42 @@ const App= () =>{
    
    setTasks(newTasks);
  };
+
+ const handleTaskDeletion = (taskId) =>{
+   const newTasks = tasks.filter(task => task.id !== taskId)
+   setTasks(newTasks)
+ };
  
  return ( 
- 
-    <>
 
-            <div className="container">
-              <AddTask handleTaskAddition={handleTaskAddition}/>
-              <Tasks tasks={tasks} handleTaskClick={handleTaskClick} />
-     
-   </div>
+  <>    
+        <div className="container">
+        
+          <Header/>
+          <AddTask handleTaskAddition={handleTaskAddition}/>
+              <Tasks
+                tasks={tasks}
+                handleTaskClick={handleTaskClick}
+                handleTaskDeletion={handleTaskDeletion}
+              
+              />
+        </div>
+
+  </>
+        
 
 
 
 
- </>)
+
+
+
+
+
+
+
+
+    );
 
  };
 export default App;
